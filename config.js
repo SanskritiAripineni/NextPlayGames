094704fba80946b57eaa18aedbd7e5b62daf5268
SanskritiAripineni <aripinenisanskriti@gmail.com>
2026-02-20 22:40:33 -0600

    Allow config.js and update Supabase public anon keys

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://bqqsmgvpwysyvtnzgpbl.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxcXNtZ3Zwd3lzeXZ0bnpncGJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NDI1MDYsImV4cCI6MjA4NzIxODUwNn0.s_bu0ErfZCBPt2K1mL0aF700bwLUuJKWLtizwU6Pf_A",
+};
+
